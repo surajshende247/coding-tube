272bfa49d169ca5161648467f00e0cc8a7790c6d
Suraj Shende <surajshende247@gmail.com>
2022-05-06 06:48:07 +0530

    add static resource loading

--- server/index.js
+++ server/index.js
@@ -48,6 +48,14 @@ app.post('/videos/add', async(req, res) => {
  
 });
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+  });
+}
+
 app.listen(5000, () =>{
     console.log('Server is running on port 5000');
 });
